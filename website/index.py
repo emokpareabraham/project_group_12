ae97384dc67212e7f5ed2ec444f97e25f2a3a744
esther2014 <esther20141108@gmail.com>
2024-04-17 21:25:53 -0400

    pan-0417 09:25

--- website/index.py
+++ website/index.py
@@ -4,9 +4,29 @@ import os
 import datetime
 
 
-#print(os.getcwd())
-
 app=Flask(__name__,static_folder='assets')
+
+
+def calculate_age(birthday):
+    age = 0
+    current_date = datetime.date.today()
+    current_year, current_month,current_day  = current_date.year, current_date.month,current_date.day
+
+    birthday_month, birthday_day, birthday_year = birthday.split('/')
+    birthday_year = int(birthday_year)
+    birthday_month = int(birthday_month)
+    birthday_day = int(birthday_day)
+
+    if birthday_month > current_month:
+        age = current_year-birthday_year-1
+    elif birthday_month == current_month:
+        if birthday_day >current_day:
+            age = current_year-birthday_year-1
+    else:
+        age = current_year-birthday_year
+    return age
+
+app.jinja_env.globals.update(calculate_age=calculate_age)
 
 PER_PAGE = 10
 
@@ -22,15 +42,16 @@ def about():
 def data():
     page = request.args.get('page', 1, type=int)
     PER_PAGE = request.args.get('per_page',10, type=int)
+    
+    
     start = (page - 1) * PER_PAGE
 
     end = start + PER_PAGE
 
-    print(start, end)
-
     con = sqlite3.connect(r'database\Football.db')
     cursor = con.cursor()
     players = cursor.execute(f"SELECT * FROM players limit {start}, {PER_PAGE}").fetchall()
+
     total_number = cursor.execute("SELECT COUNT(*) FROM players").fetchone()[0] #rows
     total_page = int(total_number/PER_PAGE)
     
